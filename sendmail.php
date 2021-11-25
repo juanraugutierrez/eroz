3884bf22a47ae9f32e7acb7f62f675cfc925757b
Juan Gutiérrez Calquín <juan.gutierrezcalquin@gmail.com>
2021-11-25 11:06:13 -0300

    64
    proyecto final

---
+++ sendmail.php
@@ -0,0 +1,312 @@
+<!DOCTYPE html>
+<html lang="es-CL">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="keywords" content="Guardia de Seguridad. Guardia, Guardias, Alarmas, Alarma,Empresas seguridad privada, empresas de seguridad, empresas de guardias de seguridad, guardias de seguridad, empresas de seguridad privada en chile, emprersas seguridad santiago, empresas de seguridad chile, servicios de guardia de seguridad, seguridad integral, seguridad, SEGURIDAD, monitoreo, monitoreo a distancia, control de acceso, vigilante privado, drones, dron, monitoreo con drones,vigilacia de predios agricolas, instalaciones de ingenieria, instacion de camaras">
+    <meta name="description" content="Guardias de seguridad y seguridad integral. Eroz Seguridad es una empresa líder enfocada en la excelencia, satisfacción del cliente, calidad y mejora continua de sus servicios en materias inherentes a seguridad privada, generando lazos de confianza y respeto con los clientes, demostrando el compromiso con el cumplimiento de las disposiciones legales vigentes, otorgando la protección y resguardo de personas (empleados-clientes), documentación, recintos, instalaciones y bienes particulares">
+    <meta name="title" content="Eroz Seguridad - Expertos en Seguridad.">
+    <meta name="description" content="Eroz Seguridad - Expertos en Seguridad.">
+    <meta name="Classification" content="General">
+    <meta name="RATING" content="General">
+    <meta name="Distribution" content="Global">
+    <meta name="language" content="Spanish">
+    <meta http-equiv="Expires" content="0">
+    <meta name="author" content="Adegest Spa">
+    <meta http-equiv="refresh" content="4;url=https://erozseguridad.cl">
+    <title>Eroz Seguridad</title>
+    <link rel="icon" type="image/png" href="img/logo-dark.png" sizes="64x64">
+    <link rel="stylesheet" href="css/components.css">
+    <link rel="stylesheet" href="css/icons.css">
+    <link rel="stylesheet" href="css/responsee.css">
+    <link rel="stylesheet" href="owl-carousel/owl.carousel.css">
+    <link rel="stylesheet" href="owl-carousel/owl.theme.css">     
+    <link rel="stylesheet" href="css/template-style.css">
+    <link href='https://fonts.googleapis.com/css?family=Playfair+Display&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700,800&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    <script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
+    <script type="text/javascript" src="js/jquery-ui.min.js"></script>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <script type="text/javascript">
+      (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "98vhzo9axy");
+  </script>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-P7FPV7H72K"></script>
+  <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-P7FPV7H72K');
+  </script>     
+  </head>
+  
+  <body class="size-1140">
+  	<!-- PREMIUM FEATURES BUTTON -->
+  
+    <!-- HEADER -->
+    <header role="banner" class="position-absolute">    
+      <!-- Top Navigation -->
+      <nav class="background-transparent background-transparent-hightlight full-width sticky">
+        <div class="s-12 l-2">
+          <a href="index.html" class="logo">
+            <!-- Logo White Version -->
+            <img class="logo-white" src="img/logo.png" alt="">
+            <!-- Logo Dark Version -->
+            <img class="logo-dark" src="img/logo-dark.png" alt="">
+          </a>
+        </div>
+        <div class="top-nav s-12 l-10">
+          <p class="nav-text"></p>
+          <ul class="right chevron">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="guardias.html">Guardias</a></li>
+            <li><a href="servicios.html">Servicios</a></li>
+            <li><a href="quienessomos.html">Quienes Somos</a></li>
+            <li><a href="drones.html">Drones</a></li>
+            <li><a href="contacto.html">Contacto</a></li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+    
+    <!-- MAIN -->
+    <main role="main">
+      <!-- Content -->
+      <article>
+        <header class="section background-dark">
+          <div class="line">        
+            <h1 class="text-white margin-top-bottom-40 text-size-60 text-line-height-1">Contactenos</h1>
+            <p class="margin-bottom-0 text-size-16">Si necesita nuestros servicios o tiene alguna duda pongase en contacto con nosotros.<br>
+            Agende una reunion, cotice nuestros servicios, entregenos sus requerimientos. <b>Eroz Seguridad Spa</b> dispone de un equipo de ejecutivos dispuestos a respoder sus requerimientos.</p>
+          </div>  
+        </header>
+        <div class="section background-white"> 
+          <div class="line margin-bottom-60">
+            <div class="margin">
+              <div class="s-12 m-12 l-4 margin-m-bottom">
+                <div class="padding-2x block-bordered border-radius">
+                  <div class="float-left hide-s">
+                    <i class="icon-sli-location-pin icon3x text-primary"></i>
+                  </div>
+                <div class="margin-left-70 margin-s-left-0 margin-bottom">
+                  <h3 class="margin-bottom-0">Direccion</h3>
+                  <p>Gran Avenida José Miguel Carrera 516<br>
+                     Oficina 312, San Miguel</p>               
+                </div>
+                </div>
+              </div>
+              <div class="s-12 m-12 l-4 margin-m-bottom">
+                <div class="padding-2x block-bordered border-radius">
+                  <div class="float-left hide-s">
+                    <i class="icon-sli-envelope icon3x text-primary"></i>
+                  </div>
+                  <div class="margin-left-70 margin-s-left-0 margin-bottom">
+                    <h3 class="margin-bottom-0">E-mail</h3>
+                    <p>contacto@erozseguridad.cl<br>
+                      ventas@erozseguridad.cl<br>
+                      info@erozseguridad.cl
+                    </p>              
+                  </div>
+                </div>
+              </div>
+              <div class="s-12 m-12 l-4 margin-m-bottom">
+                <div class="padding-2x block-bordered border-radius">
+                  <div class="float-left hide-s">
+                    <i class="icon-sli-phone icon3x text-primary"></i>
+                  </div>
+                  <div class="margin-left-70 margin-s-left-0">
+                    <h3 class="margin-bottom-0">Numeros Telefonicos</h3>
+                    <p><span class="text-primary">+569 9845 3758</span><br>
+                                                  +569 9671 0370<br>
+                       
+                    </p>             
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="line margin-bottom-60">
+          <div class="margin">
+          <div class="s-12 m-12 l-4 margin-m-bottom">
+                <div class="padding-2x block-bordered border-radius">
+                  <div class="float-left hide-s">
+                    <i class="icon-sli-phone icon3x text-primary"></i>
+                  </div>
+                  <div class="margin-left-70 margin-s-left-0">
+                <h2 class="margin-bottom-10">Eroz Comunicaciones</h2>
+                              <?php 
+                              if ($_POST['g-recaptcha-response'] == '') {
+                                echo "Captcha Invalido";
+                                exit;
+                                } else {
+                                $obj = new stdClass();
+                                $obj->secret = "6LfaC_scAAAAABFQxORdbFaZxQ2teCCwEea2vlQZ";
+                                $obj->response = $_POST['g-recaptcha-response'];
+                                $obj->remoteip = $_SERVER['REMOTE_ADDR'];
+                                $url = 'https://www.google.com/recaptcha/api/siteverify';
+                                
+                                $options = array(
+                                'http' => array(
+                                'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+                                'method' => 'POST',
+                                'content' => http_build_query($obj)
+                                )
+                                );
+                                $context = stream_context_create($options);
+                                $result = file_get_contents($url, false, $context);
+                                
+                                $validar = json_decode($result);
+                              }
+
+                              $hoy = getdate();
+
+                              $mensaje="<p><span><b>Eroz Seguridad</b></span><br><br>Contacto  Desde la WEB<br> <br></p>";
+
+                              $mensaje.="<b><br>Nombre   : </b>".$_REQUEST["name"];
+                              $mensaje.="<b><br>Email    : </b>".$_REQUEST["email"];
+                              $mensaje.="<b><br>Telefono : </b>".$_REQUEST["phone"];
+                              $mensaje.="<b><br>Asunto   : </b>".$_REQUEST["asunto"];
+
+
+                              $mensaje.="<hr /><br><p>".$_REQUEST["message"]."</p><hr />";
+
+                              $mensaje.="<br>Fecha del Mensaje : ".date("d") . "-" . date("m") . "-" . date("Y");
+
+                              $headers = "MIME-Version: 1.0\n"; 
+
+                              $headers .= "Content-type: text/html; charset=iso-8859-1\n"; 
+
+                              $headers .= "From: Contacto WEB:  <".$_REQUEST["email"].">";
+
+
+                              $dirigido = "juan.gutierrezcalquin@gmail.com";
+                              mail($dirigido , ""."Eroz Seguridad", $mensaje, $headers);
+
+                              $dirigido = "contacto@erozpropiedades.cl";
+                              mail($dirigido , "".$_REQUEST["asunto"], $mensaje, $headers);
+
+                              // $dirigido = "juan.gutierrezcalquin@gmail.com";
+
+                              // mail($dirigido, "".$_REQUEST["asunto"], $mensaje, $headers);
+                              echo "<hr/><div style='border-rigth: .3em solid blue;'>" ;
+                              echo "<p><b>Mensaje enviado</b></p>";
+                              echo "<div class='margin-left-70 margin-s-left-0'><h3 class='margin-bottom-0'>".$_REQUEST["name"]."</p>";
+                              echo "<p><span class='text-primary'>Gracias por comunicarse con nosotros, a la brevedad tomaremos contacto con usted</span><br></p>  ";
+                              ?> 
+
+                            </div>
+              </div>
+              </div>
+              </div>
+              </div>
+              <div class="s-12 m-12 l-4">
+                <h2 class="margin-bottom-10">Horario de Oficina</h2>
+                <div class="s-6">
+                  <p class="text-size-16">
+                  <b>Lunes a Jueves</b><br>
+                  <b>Viernes</b>                   
+                  </p>
+                </div>
+                <div class="s-6">
+                  <p class.="text-size-16 text-strong">
+                  09:00-18:00 hrs.<br>
+                  09:00-17:00 hrs.                  
+                  </p>
+                </div>
+              </div>
+            </div>    
+          </div>  
+        </div> 
+      </article>
+    </main>
+    
+    <!-- FOOTER -->
+    <footer>
+      <!-- Contact Us -->
+      <div class="background-primary padding text-center">
+        <p class="h1" style="font-family: ui-monospace;font-size: 1.5rem;">Contactenos: +569 9845 3758</p>
+        <p class="h3" style="font-family: ui-monospace;font-size: 1.5rem;"><a data-auto-recognition="true" id="mail"  href="mailto:contacto@erozseguridad.cl">contacto@erozseguridad.cl</a></p>
+                                                                                
+      </div>
+      
+      <!-- Main Footer -->
+      <section class="background-dark full-width">
+        <!-- Map -->
+        <div class="s-12 m-12 l-6 margin-m-bottom-2x">
+          <div class="s-12 grayscale center">  	  
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3327.0295998097367!2d-70.65646708523467!3d-33.500607007473384!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662dac581bb5df3%3A0xd9c28abf49d24c79!2sGran%20Avenida%20Jose%20Miguel%20Carrera%205161%2C%20Oficina%20312%2C%20San%20Miguel%2C%20Regi%C3%B3n%20Metropolitana!5e0!3m2!1ses!2scl!4v1634707449264!5m2!1ses!2scl" width="100%" height="450" frameborder="0" style="border:0"></iframe>
+          </div>
+        </div>
+        
+        <!-- Collumn 2 -->
+        <div class="s-12 m-12 l-6 margin-m-bottom-2x">
+          <div class="padding-2x">
+            <div class="line">              
+              <div class="float-left">
+                  <i class="icon-sli-location-pin text-primary icon3x"></i>
+                </div>
+                <div class="margin-left-70 margin-bottom-30">
+                  <h3 class="margin-bottom-0">Direccion</h3>
+                  <p>Gran Avenida José Miguel Carrera 5161<br>
+                    Oficina 312, San Miguel
+                  </p>               
+                </div>
+                <div class="float-left">
+                  <i class="icon-sli-envelope text-primary icon3x"></i>
+                </div>
+                <div class="margin-left-70 margin-bottom-30">
+                  <h3 class="margin-bottom-0">E-mail</h3>
+                  <p>contacto@erozeguridad.cl<br>
+                  
+                  </p>              
+                </div>
+                <div class="float-left">
+                  <i class="icon-sli-phone text-primary icon3x"></i>
+                </div>
+                <div class="margin-left-70">
+                  <h3 class="margin-bottom-0">Numeros Telefonicos</h3>
+                  <p>+569 9845 3758<br>
+                     +569 9671 0370<br>
+                   </p>             
+                </div>
+                <br>
+                <div class="float-left">
+                  <i class="icon-sli-share text-primary icon3x"></i>
+                </div>
+                <div class="margin-left-70">
+                  <h3 class="margin-bottom-0">Enlaces de importancia</h3>
+                  <p><ul>
+                    <li><a href="https://www.zosepcar.cl/OS10.php">OS-10</a></li>
+                    <li><a href="https://www.seguridadpublica.cl/">Subsecretario de Seguridad Publica</a></li>
+                    <li><a href="https://www.seguridadpublica.cl/red-nacional-de-seguridad-publica/">Red Nacional de Seguridad Pública</a></li>
+                    <li><a href="http://www.fnsp.gov.cl/">Fondo Nacional de Seguridad Pública</a></li>
+                    <li><a href="http://cead.spd.gov.cl/estadisticas-delictuales/">Centro de Estudios y Analisis del Delito</a></li>
+                    <li><a href="https://pazciudadana.cl">Fundacion Paz Ciudadana</a></li>  
+                  </ul>
+                   </p>             
+                </div>
+            </div>
+          </div>
+        </div>  
+      </section>
+      <hr class="break margin-top-bottom-0" style="border-color: rgba(0, 38, 51, 0.80);">
+      
+      <!-- Bottom Footer -->
+      <section class="padding background-dark full-width">
+        <div class="s-12 l-6">
+          <p class="text-size-12">Copyright 2021, ErozSeguridad Spa</p>
+          <!-- <p class="text-size-12">All images have been purchased from Bigstock. Do not use the images in your website.</p> -->
+        </div>
+        <div class="s-12 l-6">
+          <a class="right text-size-12" href="http://www.adegest.cl" title="Responsee - lightweight responsive framework">Diseñado <br> por Adegest</a>
+        </div>
+      </section>
+    </footer>
+    <script type="text/javascript" src="js/responsee.js"></script>
+    <script type="text/javascript" src="owl-carousel/owl.carousel.js"></script>
+    <script type="text/javascript" src="js/template-scripts.js"></script>
+  </body>
+</html>
